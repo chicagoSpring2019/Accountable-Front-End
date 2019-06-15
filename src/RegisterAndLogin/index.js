b8e1931c6566e2036ba15a4f383c4b38015f4451
Greg Gancarz <propterprivatum@tuta.io>
2019-06-15 11:36:52 -0500

    feat(Login/Registration components): Now recieves appropriate messages for login/reg failures
    - User now recieves a message from the back or front reflecting the problem w/ their login/reg
    attempt
    - messages include things like unmet password length, incorrect username/password, etc
    -
    created new filler text to remain in place when message is not present, so that the text does not
    jump around when the message does appear

--- src/RegisterAndLogin/index.js
+++ src/RegisterAndLogin/index.js
@@ -8,11 +8,20 @@ class RegisterAndLogin extends React.Component {
 			email: '',
 			password: '',
 			showRegister: false,
+			message: '',
+			showMessage: false,
 		}
 	}
 
 	handleChange = (e) => {
 		this.setState({[e.target.name]: e.target.value});
+	}
+
+	showMessage = () => {
+		console.log(" ");
+		this.setState({
+			showMessage: true
+		})
 	}
 
 	clearForm = () => {
@@ -32,15 +41,16 @@ class RegisterAndLogin extends React.Component {
 	hideRegisterForm = () => {
 		this.clearForm();
 		this.setState({
-			showRegister: false
+			showRegister: false,
+			showMessage: false,
 		})
 	}
 
 	handleLogin = async (e) => {
 		e.preventDefault();
 		console.log('--handleLogin() initiated--');
+		this.showMessage();
 		try {
-
 			const bodyToSend = {
 				email: this.state.email,
 				password: this.state.password
@@ -58,11 +68,17 @@ class RegisterAndLogin extends React.Component {
 		  	console.log(loginResponse, "<<--- loginResponse");
 		  	const parsedResponse = await loginResponse.json();
 		  	console.log(parsedResponse, "<=-=-= parsedResponse");
-		  	if(parsedResponse.status === 200) {
+		  	if (parsedResponse.status === 200) {
 				console.log(parsedResponse.data, '<<< parsedResponse.data in handleLogin()');
 				await this.props.setActiveUserEmailAndLogged(parsedResponse.data.email);
 				this.props.setActiveUserId(parsedResponse.data._id);
-
+			} else if (parsedResponse.status === 404) {
+				console.log(parsedResponse.message, '<<< parsedResponse.message in handleLogin()');
+				this.setState({
+					showMessage: true,
+					message: parsedResponse.message
+				})
+				console.log(this.state.message, "<<<state message");
 		  	}
 		} catch(err) {
 		  	console.log(err);
@@ -72,26 +88,35 @@ class RegisterAndLogin extends React.Component {
 	handleRegister = async (e) => {
 		e.preventDefault();
 		console.log('--handleRegister() has been initiated--');
-		try {
-			const registerResponse = await fetch(process.env.REACT_APP_BACKEND_URL + 'auth/register',  {
-				method: 'POST',
-				credentials: 'include',
-				body: JSON.stringify(this.state),
-				headers: {
-					'Content-Type': 'application/json'
+		if (this.state.password.length >= 6) {
+			try {
+				const registerResponse = await fetch(process.env.REACT_APP_BACKEND_URL + 'auth/register',  {
+					method: 'POST',
+					credentials: 'include',
+					body: JSON.stringify(this.state),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+				this.clearForm();
+				const parsedResponse = await registerResponse.json();
+				console.log(parsedResponse, "<----  parsedResponse in handleRegister()");
+				if(parsedResponse.status === 200) {
+					console.log(parsedResponse.data, '<<< parsedResponse.data in handleRegister()');
+					await this.createDefaultCats(parsedResponse.data._id);
+					this.props.setActiveUserEmailAndLogged(parsedResponse.data.email);
+					this.props.setActiveUserId(parsedResponse.data._id);
 				}
-			})
-			this.clearForm();
-			const parsedResponse = await registerResponse.json();
-			console.log(parsedResponse, "<----  parsedResponse in handleRegister()");
-			if(parsedResponse.status === 200) {
-				console.log(parsedResponse.data, '<<< parsedResponse.data in handleRegister()');
-				await this.createDefaultCats(parsedResponse.data._id);
-				this.props.setActiveUserEmailAndLogged(parsedResponse.data.email);
-				this.props.setActiveUserId(parsedResponse.data._id);
+			} catch (err) {
+				console.log(err);
 			}
-		} catch (err) {
-			console.log(err);
+		} else {
+			console.log("--password was too short--");
+
+			this.setState({
+				showMessage: true,
+				message: "You're password must be six characters in length or more"
+			})
 		}
 	}
 
@@ -119,6 +144,11 @@ class RegisterAndLogin extends React.Component {
 	}
 
 	render() {
+		const Message = (
+			<p>
+				{this.state.message}
+			</p>
+		)
 
 		const Login = (
 			<div>
@@ -128,6 +158,7 @@ class RegisterAndLogin extends React.Component {
 					<input type='text' name='email' value={this.state.email} onChange={this.handleChange}/>
 					Password:
 					<input type='password' name='password' value={this.state.password} onChange={this.handleChange}/>
+					{this.state.showMessage ? Message : <p>Should have the same diameters as the message div</p>}
 					<button> Log in </button>
 				</form>
 				<p> Don't have an account? Set one up now! It's free and easy. </p>
@@ -139,21 +170,20 @@ class RegisterAndLogin extends React.Component {
 			<div>
 				<h3 >Registration </h3>
 				<form id="register-form" onSubmit={this.handleRegister}>
-					<wrapper className="logForm">
-					Email:
-					<input type='text' name='email' value={this.state.email} onChange={this.handleChange}/>
-					</wrapper>
+					<div className="logForm">
+						Email:
+						<input type='email' name='email' value={this.state.email} onChange={this.handleChange}/>
+					</div>
 
-					<wrapper className="logForm">
-					Password:
-					<input type='password' name='password' value={this.state.password} onChange={this.handleChange}/>
-					</wrapper>
-
-
+					<div className="logForm">
+						Password:
+						<input type='password' name='password' value={this.state.password} onChange={this.handleChange}/>
+					</div>
+					{this.state.showMessage ? Message : <p>Should have the same diameters as the message div</p>}
 					<button type='sumbit'>Register</button>
 				</form>
 				<p> You have an account after all? </p>
-				<button onClick={this.hideRegisterForm}> Login </button>
+				<button onClick={this.hideRegisterForm} > Login </button>
 			</div>
 		)
 
