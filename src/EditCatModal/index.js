63c4a1496b76632b2cca80516e5e37b7ff7da1f6
Greg Gancarz <propterprivatum@tuta.io>
2019-07-21 12:13:06 -0500

    Created new modal for editing cats

---
+++ src/EditCatModal/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button, Modal, Form } from 'semantic-ui-react';
+
+class EditCatModal extends React.Component {
+	constructor(props) {
+		super();
+		this.state = {
+
+		}
+	}
+}
+
+
+
+
+export default EditCatModal
